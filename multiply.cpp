258dd9f237d8c24955ceb7c710493d7db0d3f842
mightysabean <findthebeginningfirst@gmail.com>
2017-09-14 12:54:26 +0100

    Modified the multiply program to take console input

--- multiply.cpp
+++ multiply.cpp
@@ -4,21 +4,32 @@
 // A simple multiply program using a function
 using namespace std;
 
-double multiply(double numb1, double numb2);
+double numb3[1],numb1,numb2;
+double multiply(double numb1,double numb2);
+
 
 int main() {
 
 	
-	multiply(5.3, 20.9);
+	multiply(numb1, numb2);
 
 	return 0;
 }
 
-double multiply(double numb1, double numb2) {
+double multiply(double numb1,double numb2) {
+	cout << "Enter two numbers to multiply" << endl;
 
+	for (int i = 0; i < 2; i++) {
+		cout << "Number " << i + 1 << endl;
+		cin >> numb3[i];
+
+
+	}
+	numb1 = numb3[0];
+	numb2 = numb3[1];
 	double result=numb1*numb2;
 
-	cout << result << endl;
+	cout <<"The result is "<< result << endl;
 	return result;
 
 
